203ddbdb377c5fa6b4676b31a7ff82fbba8b9ec9
Flowdeeps <pete@republicofpete.net>
2013-06-28 01:05:26 +0200

    Working game loop

--- js/game.js
+++ js/game.js
@@ -1,1 +1,42 @@
 // game logic
+
+window.onload = function(){
+  // gameloop
+
+  // requestAnim shim layer by Paul Irish
+  window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       || 
+            window.webkitRequestAnimationFrame || 
+            window.mozRequestAnimationFrame    || 
+            window.oRequestAnimationFrame      || 
+            window.msRequestAnimationFrame     || 
+            function(/* function */ callback, /* DOMElement */ element){
+              window.setTimeout(callback, 1000 / 60);
+            };
+  })();
+
+  init();
+  animate();
+
+  function init(){
+    // get canvas gubbins
+    var canvas    = document.createElement('canvas');
+    var context   = canvas.getContext('2d');
+
+    // set some stuff
+    canvas.width  = 800;
+    canvas.height = 600;
+
+    document.body.appendChild(canvas);
+
+  }
+
+  var fps = 60;
+  function animate() {
+    setTimeout(function() {
+      requestAnimationFrame(animate);
+      // Drawing code goes here
+    }, 1000 / fps);
+  }
+
+};
